26e5493b6be1d1fdc5a44782c4d163d60380cc4c
Emmanuel <mezamartinezzz@ciencias.unam.mx>
2016-12-15 02:20:04 -0600

    pagina principal y ubicacion

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Candy</title>
+	<meta charset='utf-8' />
+	<style type="text/css">
+	
+	 #menu {
+	 	background-color:#ADEAEA;
+	 	border: 10px pink solid;
+	 	font-size: 30px;
+
+	 }
+
+	 </style>
+</head>
+
+<body background="imagenes/fondo.jpg">
+
+   <center>
+   <table id='menu'>
+   <tr>
+       <td id='lista1'>Inicio | </td>
+       <td id='lista2'>Productos | </td>
+       <td id='lista3'><td><a href='ubicacion.php'>Ubicacion | </a></td>
+       <td id='lista4'>Contacto | </td>
+       <td id='lista5'>Administrador | </td> 
+   </tr>
+   	
+   <img src="imagenes/vanelope.png" width="450" height="400" align="left">
+   <img src="imagenes/Ralph.png" width="500" height="400" align="right">
+   
+   </table>
+
+   </center>
+
+</body>
+</html>
---
+++ ubicacion.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Candy</title>
+	<meta charset='utf-8' />
+	<style type="text/css">
+	
+	 #menu {
+	 	background-color:#ADEAEA;
+	 	border: 10px pink solid;
+	 	font-size: 30px;
+
+	 }
+
+	 </style>
+</head>
+
+<body background="imagenes/fondo.jpg">
+
+   <center>
+   <table id='menu'>
+   <tr>
+       <td id='lista1'><td><a href='index.php'>Inicio | </a></td>
+       <td id='lista2'>Productos | </td>
+       <td id='lista3'><td><a href='ubicacion.php'>Ubicacion | </a></td>
+       <td id='lista4'>Contacto | </td>
+       <td id='lista5'>Administrador | </td> 
+   </tr>   
+   </table>
+
+   <iframe src="https://www.google.com/maps/embed?pb=!1m0!3m2!1ses-419!2smx!4v1479173947402!6m8!1m7!1sM8JWO710ZFDoKIjPUpxTGQ!2m2!1d19.30431586218952!2d-99.19115536136918!3f296.9223468501694!4f-2.9839097928340976!5f0.7820865974627469" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+   </center>
+
+</body>
+</html>
